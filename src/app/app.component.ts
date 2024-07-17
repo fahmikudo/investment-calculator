ee6bc28f8aa24a1a397d188f392ade2f19f9fdb0
Fahmi Kudo <fahmi.hidayatullah12@gmail.com>
2024-07-17 11:36:07 +0700

    learn angular with project investment calcultor

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    standalone: false,
+    templateUrl: './app.component.html',
+})
+export class AppComponent {
+    resultData = signal<
+        | {
+              year: number;
+              interest: number;
+              valueEndOfYear: number;
+              annualInvestment: number;
+              totalInterest: number;
+              totalAmountInvested: number;
+          }[]
+        | undefined
+    >(undefined);
+}
